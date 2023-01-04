9620019bae16f2974ec47e49ab896b200c9a8282
EstefanoEspinoza <90330394+EstefanoEspinoza@users.noreply.github.com>
2023-01-04 20:24:17 -0300

    Add files via upload

--- utils.cu
+++ utils.cu
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <cuda.h>
 #include <cstdlib>
@@ -6,21 +5,17 @@
 #include <omp.h>
 
 void printAutomataCelular(int n,bool *board){
-     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++) {
-            printf(board[i*n+j] ? "*" : "/");
-        }
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++) printf(board[i*n+j] ? "*" : "=");
         printf("\n");
     }
 }
 
 
-
-
 int count_live_neighbour_cell(bool *board,int n, int r, int c){
     int i, j, count=0;
-    for(i = r - 1; i < = r + 1; i++){
-        for(j = c - 1; j < = c + 1; j++){
+    for(i=r-1; i<=r+1; i++){
+        for(j=c-1;j<=c+1;j++){
             if((i==r && j==c) || (i<0 || j<0) || (i>=n || j>=n)){
                 continue;
             }
@@ -31,7 +26,5 @@ int count_live_neighbour_cell(bool *board,int n, int r, int c){
     }
     return count;
 }
-
-
 
 //Función sacada directamente desde el enlace que dejó en el .txt de la prueba 3, puesta por recomendación de un compañero.
